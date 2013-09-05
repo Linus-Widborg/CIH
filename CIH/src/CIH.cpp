0c609dd002a3015231ca46743f9f31a1a5317463
linus <linus.widborg@gmail.com>
2013-09-05 11:49:09 +0200

    Improved the game print output.

--- CIH/src/CIH.cpp
+++ CIH/src/CIH.cpp
@@ -14,14 +14,19 @@ int main() {
 	cout << "Welcome to CIH" << endl;
 	Deck deck;
 	Card rink[12];
-	cout << "GK LD RD LW CE RW" << endl;
-	for (int i = 0; i < 12; i++) {
-		rink[i] = deck.getRandomCard();
-		cout << rink[i].toString() << " ";
-		if (i == 6) {
-			cout << endl;
+	cout << "GK  LD  RD  LW  CE  RW " << endl;
+	cout << "-----------------------" << endl;
+	while (deck.numberOfCards() > 12) {
+		for (int i = 0; i < 12; i++) {
+			rink[i] = deck.getRandomCard();
+			cout.width(3);
+			cout << left << rink[i].toString() << " ";
+			if (i == 5) {
+				cout << endl;
+			}
 		}
+		cout << endl;
+		cout << "=======================" << endl;
 	}
-	cout << endl;
 	return 0;
 }
