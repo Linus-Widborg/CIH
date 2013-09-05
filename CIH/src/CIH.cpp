380205e4f925ab9fd91d54f8b9e2e868ef84a70c
linus <linus.widborg@gmail.com>
2013-09-05 11:23:27 +0200

    Print rink.

--- CIH/src/CIH.cpp
+++ CIH/src/CIH.cpp
@@ -14,9 +14,14 @@ int main() {
 	cout << "Welcome to CIH" << endl;
 	Deck deck;
 	Card rink[12];
+	cout << "GK LD RD LW CE RW" << endl;
 	for (int i = 0; i < 12; i++) {
 		rink[i] = deck.getRandomCard();
-		//cout << rink[i].getColor() << rink[i].getValue
+		cout << rink[i].toString() << " ";
+		if (i == 6) {
+			cout << endl;
+		}
 	}
+	cout << endl;
 	return 0;
 }
